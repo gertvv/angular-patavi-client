75013f1d8b7fb3ba70b9fbcd1917602ecce2f67d
Connor Stroomberg <c.stroomberg@gmail.com>
2016-06-15 11:24:01 +0200

    Work-around for chrome wss bug, see https://bugs.chromium.org/p/chromium/issues/detail?id=329884 Send warmup request to patavi server, Only after the warmup request has completed do the wss request

--- patavi.js
+++ patavi.js
@@ -5,7 +5,9 @@ define(['angular'], function(angular) {
     // uriOrPromise: websocket URI or a promise resolving to a websocket URI
     // returns: a promise for the task results, which also sends notifications
     var listenForUpdates = function(uriOrPromise) {
-      var uriPromise = uriOrPromise.then ? uriOrPromise : $q(function(resolve) { resolve(uriOrPromise); });
+      var uriPromise = uriOrPromise.then ? uriOrPromise : $q(function(resolve) {
+        resolve(uriOrPromise);
+      });
 
       var resultsPromise = $q.defer();
 
@@ -15,21 +17,35 @@ define(['angular'], function(angular) {
         });
       }
 
+      // send a one time 'warm-up request to wws server as a workaround for chrome bug'
+      // see https://bugs.chromium.org/p/chromium/issues/detail?id=329884 for details
+      function warmup(uri) {
+        return $http({
+          method: 'GET',
+          url: uri.replace('wss', 'https') + '/warm-up'
+        });
+      }
+
       uriPromise.then(function(uri) {
-        var socket = new WebSocket(uri);
-        socket.onmessage = function (event) {
-          var data = JSON.parse(event.data);
-          if (data.eventType === "done") {
-            socket.close();
-            getResults(data.eventData.href, resultsPromise.resolve);
-          } else if (data.evenType === "failed") {
-            socket.close();
-            getResults(data.eventData.href, resultsPromise.reject)
-          }
-          resultsPromise.notify(data);
-        }
+        warmup(uri).finally(function() {
+          var socket = new WebSocket(uri);
+          socket.onmessage = function(event) {
+            var data = JSON.parse(event.data);
+            if (data.eventType === 'done') {
+              socket.close();
+              getResults(data.eventData.href, resultsPromise.resolve);
+            } else if (data.evenType === 'failed') {
+              socket.close();
+              getResults(data.eventData.href, resultsPromise.reject);
+            }
+            resultsPromise.notify(data);
+          };
+        });
       }, function(error) {
-        resultsPromise.reject({ 'status': 'error', 'error': error });
+        resultsPromise.reject({
+          'status': 'error',
+          'error': error
+        });
       });
 
       return resultsPromise.promise;
